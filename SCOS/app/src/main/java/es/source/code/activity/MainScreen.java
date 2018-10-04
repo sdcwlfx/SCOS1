c6ed8eca2383732d27cbe8132402e11779fbf824
sdcwlfx <18463102717@163.com>
2018-10-04 23:09:19 +0800

    E02

---
+++ SCOS/app/src/main/java/es/source/code/activity/MainScreen.java
@@ -0,0 +1,15 @@
+package es.source.code.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.scos.R;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
